c43bc047207d0d25f7aaee2cbe120dbe4a70ed82
Sollace <sollacea@gmail.com>
2022-06-12 10:30:13 +0200

    Prevent duplicate sounds in acoustic resolution. Should fix #142

--- src/main/java/eu/ha3/presencefootsteps/sound/acoustics/AcousticsPlayer.java
+++ src/main/java/eu/ha3/presencefootsteps/sound/acoustics/AcousticsPlayer.java
@@ -1,7 +1,6 @@
 package eu.ha3.presencefootsteps.sound.acoustics;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import eu.ha3.presencefootsteps.PresenceFootsteps;
 import eu.ha3.presencefootsteps.sound.State;
@@ -27,11 +26,7 @@ public class AcousticsPlayer implements AcousticLibrary {
     @Override
     public void playAcoustic(LivingEntity location, String acousticName, State event, Options inputOptions) {
         if (acousticName.contains(",")) {
-            String[] fragments = acousticName.split(",");
-
-            for (String fragment : fragments) {
-                playAcoustic(location, fragment, event, inputOptions);
-            }
+            Set.of(acousticName.split(",")).forEach(fragment -> playAcoustic(location, fragment, event, inputOptions));
         } else if (!acoustics.containsKey(acousticName)) {
             PresenceFootsteps.logger.warn("Tried to play a missing acoustic: " + acousticName);
         } else {
