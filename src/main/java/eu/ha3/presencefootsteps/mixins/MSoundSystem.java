84ef0f8e76da680a07cf109362e69db83aa1b722
Sollace <sollacea@gmail.com>
2022-11-03 11:19:13 +0100

    Fixed crash due to incorrectly configured mixin. Closes #181

--- src/main/java/eu/ha3/presencefootsteps/mixins/MSoundSystem.java
+++ src/main/java/eu/ha3/presencefootsteps/mixins/MSoundSystem.java
@@ -1,19 +1,26 @@
 package eu.ha3.presencefootsteps.mixins;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import eu.ha3.presencefootsteps.sound.player.ImmediateSoundPlayer;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.SoundSystem;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.math.MathHelper;
 
 @Mixin(SoundSystem.class)
 abstract class MSoundSystem {
-    @ModifyConstant(method = "getAdjustedVolume", constant = @Constant(floatValue = 1F))
-    private float onGetAdjustedVolume(float constant, SoundInstance sound) {
+    @Shadow
+    abstract float getSoundVolume(@Nullable SoundCategory category);
+
+    @Inject(method = "getAdjustedVolume(Lnet/minecraft/client/sound/SoundInstance;)F", at = @At("HEAD"), cancellable = true)
+    private void onGetAdjustedVolume(SoundInstance sound, CallbackInfoReturnable<Float> info) {
         if (sound instanceof ImmediateSoundPlayer.UncappedSoundInstance t) {
-            return t.getMaxVolume();
+            info.setReturnValue(MathHelper.clamp(t.getVolume() * getSoundVolume(t.getCategory()), 0, t.getMaxVolume()));
         }
-        return constant;
     }
 }
